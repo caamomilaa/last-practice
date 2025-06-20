3fd37d239ae773c57d6b102fc02b6395d77924fd
MacarenaCarrero <c.velazquezdamico@gmail.com>
2025-06-20 12:37:30 +0200

    updae

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,9 +1,26 @@
+import { useContext, useEffect, useState } from "react";
+import Title from "../../components/ui/titles/Title";
+import { AuthContext } from "../../lib/context/AuthContext";
+
 const Profile = () => {
+
+	const [isEditing, setIsEditing]= useState()
+
+	useEffect(()=>{
+	},[])
+	
+	const { user } = useContext(AuthContext);
 	return (
 		<>
+			<Title type={'l'}>Profile</Title>
 			<p>Foto bonita</p>
+			<p>Name:</p>
+			{/* <p>Email: {user.email}</p> */}
+			<button onClick={setIsEditing(!isEditing)}>Editar</button>
 		</>
 	);
 };
+
+//si recargo la pagina, user.email sale como null. entiendo que hay que hacer un async, pero no se donde
 
 export default Profile;
